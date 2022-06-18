e9321266553d07a3441b171dbad6a8380e2cc277
jjffbb <juanfelipebriceo@outlook.com>
2022-06-18 17:20:08 -0500

    Update site.js

--- site.js
+++ site.js
@@ -1,4 +1,33 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+function sendData(name, age){
 
-// Write your JavaScript code.
+    fetch("https://jjffbb.github.io/DySweb/Index.html", {
+        method: "POST",
+        mode: "core",
+        headers: {"Content_Type":"application/json"},
+        body: JSON.stringify({name:name, age:age})
+    })
+    .then(response => response.json()
+    .then(json => console.log(json)))
+    .catch(error => alert(error.message));
+}
+
+addEventListener("load", () => {
+    fetch("https://jjffbb.github.io/DySweb/Home.html")
+    .then(resp => resp.json())
+    .then(data =>{
+        data.forEach(element => {
+            console.log.log(element.name);
+        });
+    })
+})
+
+document.getElementById("sendInfo")
+.addEventListener("click", (evt) => {
+    evt.preventDefault();
+
+    let name = document.getElementById("name").value
+    let age = document.getElementById("age").value
+
+    sendData(name, age);
+
+})
